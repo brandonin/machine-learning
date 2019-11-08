2f391542db033c215cc77e15d77f700e8eee79cd
Brandon In <brandonin@me.com>
2019-11-07 22:28:19 -0800

    added random forest to determine mean absolute error

--- kaggle/housing.py
+++ kaggle/housing.py
@@ -27,3 +27,18 @@ iowa_model.fit(train_X, train_y)
 val_predictions = iowa_model.predict(val_X)
 val_mae = mean_absolute_error(val_predictions, val_y)
 print("Validation MAE: {:,.0f}".format(val_mae))
+
+
+# random forest
+from sklearn.ensemble import RandomForestRegressor
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+rf_val_mae = mean_absolute_error(rf_model.predict(val_X), val_y)
+
+print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
